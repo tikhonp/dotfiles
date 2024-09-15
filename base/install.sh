253889e246e5c8143d58ac3fcf831b52ff0209e7
TikhonP <tikhon.petrishchev@gmail.com>
2024-09-15 23:24:49 +0300

    First try to move to stow

--- base/install.sh
+++
@@ -1,8 +0,0 @@
-mv ~/.zshrc $DOTFILES_OLD_CONFS_PATH/zshrc
-mv ~/.zsh_profile $DOTFILES_OLD_CONFS_PATH/zsh_profile
-
-rm ~/.zshrc
-rm ~/.zsh_profile
-
-cp ./zshrc $HOME/.zshrc
-cp ./zsh_profile $HOME/.zsh_profile
