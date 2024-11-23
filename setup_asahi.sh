9c496721653ab4cc6e139c0c9debb669227f263f
TikhonP <tikhon.petrishchev@gmail.com>
2024-11-23 20:24:06 +0300

    Deleteed nvim config to properly setup submodule

--- setup_asahi.sh
+++ setup_asahi.sh
@@ -125,7 +125,7 @@ curl -fsSL https://tailscale.com/install.sh | sh
 sudo dnf -y install dnf-plugins-core
 sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl enable --now docker
+# sudo systemctl enable --now docker
 sudo usermod -a -G docker $USER # log out -> log in after
 
 # Install apple pkl
