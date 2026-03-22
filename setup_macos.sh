bdef7ec0b23c671d68e71f9f7fe5ac5a794a3bd0
TikhonP <tikhon.petrishchev@gmail.com>
2026-03-22 22:34:50 +0300

    Updated macos setup

--- setup_macos.sh
+++ setup_macos.sh
@@ -1,28 +1,29 @@
 # 1. Install Apple's Command Line Tools, which are prerequisites for Git and Homebrew.
-
 xcode-select --install
 
-
 # 2. Install Homebrew, followed by the software listed in the Brewfile.
-
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+# 3. Install shadowrocket and enable vpn, then:
 curl -s https://raw.githubusercontent.com/tikhonp/dotfiles/refs/heads/main/Brewfile | brew bundle --file=- -v
 
+# 4. Setup Bitwarden SSH Agent, enable it in settings
+#
+# After Use this to fetch dotfiles by ssh for this session:
+export SSH_AUTH_SOCK=/Users/tikhon/.bitwarden-ssh-agent.sock
 
-# 3. Create `~/projects` folder
-
+# 5. Create `~/projects` folder
 mkdir -p ~/projects
 
+# 6. Clone repo into newly created
+cd ~/projects
+git clone git@github.com:TikhonP/dotfiles.git
 
+7. Create symlinks in the Home directory to the real files in the repo.
 
-# Bitwarden
-#
-# Configure OpenSSH: Edit your SSH configuration file (~/.ssh/config) to tell the macOS SSH client to use the Bitwarden agent's socket. Add the following configuration:
+```zsh
+mkdir ~/.config # so not all dot-config will not symlinked
 
-```bash
-Host *
-    IdentitiesOnly yes
-    # Bitwarden SSH agent
-    IdentityAgent ~/.bitwarden-ssh-agent.sock
+stow --target=$HOME . --dotfiles
 ```
+
